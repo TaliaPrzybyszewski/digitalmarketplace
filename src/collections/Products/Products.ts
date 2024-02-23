80841e8715608af478fd24662bbdc0e9d765bb9b
Talia Przybyszewski <tprzybyszewski@icloud.com>
2024-02-23 00:11:44 +0000

    updating types

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -16,52 +16,121 @@ const addUser: BeforeChangeHook<Product> = async ({
   return { ...data, user: user.id }
 }
 
+const syncUser: AfterChangeHook<Product> = async ({
+  req,
+  doc,
+}) => {
+  const fullUser = await req.payload.findByID({
+    collection: 'users',
+    id: req.user.id,
+  })
+
+  if (fullUser && typeof fullUser === 'object') {
+    const { products } = fullUser
+
+    const allIDs = [
+      ...(products?.map((product) =>
+        typeof product === 'object' ? product.id : product
+      ) || []),
+    ]
+
+    const createdProductIDs = allIDs.filter(
+      (id, index) => allIDs.indexOf(id) === index
+    )
+
+    const dataToUpdate = [...createdProductIDs, doc.id]
+
+    await req.payload.update({
+      collection: 'users',
+      id: fullUser.id,
+      data: {
+        products: dataToUpdate,
+      },
+    })
+  }
+}
+
+const isAdminOrHasAccess =
+  (): Access =>
+  ({ req: { user: _user } }) => {
+    const user = _user as User | undefined
+
+    if (!user) return false
+    if (user.role === 'admin') return true
+
+    const userProductIDs = (user.products || []).reduce<
+      Array<string>
+    >((acc, product) => {
+      if (!product) return acc
+      if (typeof product === 'string') {
+        acc.push(product)
+      } else {
+        acc.push(product.id)
+      }
+
+      return acc
+    }, [])
+
+    return {
+      id: {
+        in: userProductIDs,
+      },
+    }
+  }
 
 export const Products: CollectionConfig = {
   slug: 'products',
   admin: {
     useAsTitle: 'name',
   },
-  access: {},
+  access: {
+    read: isAdminOrHasAccess(),
+    update: isAdminOrHasAccess(),
+    delete: isAdminOrHasAccess(),
+  },
   hooks: {
-    beforeChange: [addUser, async (args) => {
-      if(args.operation === "create") {
-        const data = args.data as Product
+    afterChange: [syncUser],
+    beforeChange: [
+      addUser,
+      async (args) => {
+        if (args.operation === 'create') {
+          const data = args.data as Product
 
-        const createdProduct = await stripe.products.create({
-          name: data.name,
-          default_price_data: {
-            currency: 'USD',
-            unit_amount: Math.round(data.price * 100), 
+          const createdProduct =
+            await stripe.products.create({
+              name: data.name,
+              default_price_data: {
+                currency: 'USD',
+                unit_amount: Math.round(data.price * 100),
+              },
+            })
+
+          const updated: Product = {
+            ...data,
+            stripeId: createdProduct.id,
+            priceId: createdProduct.default_price as string,
           }
-        })
 
-        const updated: Product = {
-          ...data,
-          stripeId: createdProduct.id,
-          priceId: createdProduct.default_price as string
+          return updated
+        } else if (args.operation === 'update') {
+          const data = args.data as Product
+
+          const updatedProduct =
+            await stripe.products.update(data.stripeId!, {
+              name: data.name,
+              default_price: data.priceId!,
+            })
+
+          const updated: Product = {
+            ...data,
+            stripeId: updatedProduct.id,
+            priceId: updatedProduct.default_price as string,
+          }
+
+          return updated
         }
-
-        return updated
-
-      } else if(args.operation === "update"){
-        const data = args.data as Product
-
-        const updatedProduct = await stripe.products.update(data.stripeId!, {
-          name: data.name,
-          default_price: data.priceId!
-        })
-
-        const updated: Product = {
-          ...data,
-          stripeId: updatedProduct.id,
-          priceId: updatedProduct.default_price as string
-        }
-
-        return updated
-
-      }
-    }],
+      },
+    ],
   },
   fields: [
     {
@@ -97,7 +166,9 @@ export const Products: CollectionConfig = {
       name: 'category',
       label: 'Category',
       type: 'select',
-      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
+      options: PRODUCT_CATEGORIES.map(
+        ({ label, value }) => ({ label, value })
+      ),
       required: true,
     },
     {
@@ -108,7 +179,6 @@ export const Products: CollectionConfig = {
       relationTo: 'product_files',
       hasMany: false,
     },
-
     {
       name: 'approvedForSale',
       label: 'Product Status',
